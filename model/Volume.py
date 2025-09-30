5ceae9d3ca74eb19291637e39b8e017ff2e07855
João Pinheiro <jp.joaopedro9190@gmail.com>
2025-09-30 12:39:27 -0300

    Add atributo numero na classe Volume

--- model/Volume.py
+++ model/Volume.py
@@ -4,16 +4,17 @@ from model.Capitulo import Capitulo
 
 class Volume:
     # Construtor
-    def __init__(self, nome: str, capitulos: List[Capitulo]) -> None:
+    def __init__(self, nome: str, numero: int, capitulos: List[Capitulo]) -> None:
         '''Inicia o objeto da classe Volume com um nome, número, e uma lista de capitulos.
         ARGS:
             nome (str): Nome do volume.
-            numero (float): Número do volume.
+            numero (int): Número do volume.
             capitulos (List[Capitulo]): Uma lista de capitulos do volume.
         Return:
             None: Não possuí retorno.
         '''
         self.__nome = nome
+        self.__numero = numero
         self.__capitulos = capitulos
 
     # Métodos Getter e Setter
@@ -24,6 +25,14 @@ class Volume:
     @nome.setter
     def nome(self, nome: str):
         self.__nome = nome
+
+    @property
+    def numero(self) -> int:
+        return self.__numero
+    
+    @numero.setter
+    def numero(self, numero) -> None:
+        self.__numero = numero
 
     @property
     def capitulos(self) -> List[Capitulo]:
