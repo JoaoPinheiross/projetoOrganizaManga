6fbf7d82624687b2120ee622d3a89c9b6473cf00
João Pinheiro <jp.joaopedro9190@gmail.com>
2025-09-29 23:30:30 -0300

    Definindo classe volume

--- model/Volume.py
+++ model/Volume.py
@@ -1,3 +1,24 @@
+from typing import List
+
+from model.Capitulo import Capitulo
+
 class Volume:
-    def __init__(self):
-        pass
+    def __init__(self, nome: str, capitulos: List[Capitulo]):
+        self.__nome = nome
+        self.__capitulos = capitulos
+
+    @property
+    def nome(self) -> str:
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, nome: str):
+        self.__nome = nome
+
+    @property
+    def capitulos(self) -> List[Capitulo]:
+        return self.__capitulos
+    
+    @capitulos.setter
+    def capitulos(self, capitulos: List[Capitulo]):
+        self.__capitulos = capitulos
