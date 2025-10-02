fb365c991f112e7146f562ba45f30344bcd95e7f
João Pinheiro <jp.joaopedro9190@gmail.com>
2025-10-02 15:52:20 -0300

    modif classe Manga

--- model/Manga.py
+++ model/Manga.py
@@ -1,23 +1,42 @@
-from typing import List
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-from model.Volume import Volume
+from Base import Base
+'''from model.Autor import Autor
+from model.Volume import Volume'''
 
-class Manga:
+class Manga(Base):
     # Construtor
-    def __init__(self, nome: str, autor: str, volumes: List[Volume]) -> None:
-        '''Inicia o objeto da classe Manga com um nome, autor, e uma lista de Volumes.
+    '''def __init__(self, nome: str, autor: str, volumes: List[Volume]) -> None:
+        Inicia o objeto da classe Manga com um nome, autor, e uma lista de Volumes.
         ARGS:
             nome (str): Nome do manga.
             autor (str): Nome do autor do manga.
             volumes (List[Volume]): Uma lista dos volumes do manga.
         Return:
             None: Não possuí retorno.
-        '''
+        
         self.__nome = nome
         self.__autor = autor
-        self.__volumes = volumes
+        self.__volumes = volumes'''
+    __tablename__ = "mangas"
+
+    __idManga = Column(Integer, primary_key=True, autoincrement=True)
+    __nome = Column(String(60), nullable=False)
+    __idAutor = Column(Integer, ForeignKey("autores.idAutor"), nullable=False)
+
+    autor = relationship("Autor", back_populates="mangas")
+    volumes = relationship("Volume", back_populates="manga", cascade="all, delete-orphan", order_by="Volume.numero")
 
     # Métodos Getter e Setter
+    @property
+    def idManga(self) -> int:
+        return self.__idManga
+    
+    @idManga.setter
+    def idManga(self, idManga: int) -> None:
+        self.__idManga = idManga
+    
     @property
     def nome(self) -> str:
         return self.__nome
@@ -27,17 +46,9 @@ class Manga:
         self.__nome = nome
 
     @property
-    def autor(self) -> str:
-        return self.__autor
+    def idAutor(self) -> int:
+        return self.__idAutor
     
-    @autor.setter
-    def autor(self, autor: str) -> None:
-        self.__autor = autor
-
-    @property
-    def volumes(self) -> List[Volume]:
-        return self.__volumes
-    
-    @volumes.setter
-    def volumes(self, volumes: List[Volume]) -> None:
-        self.__volumes = volumes
+    @idAutor.setter
+    def idAutor(self, idAutor: int) -> None:
+        self.__idAutor = idAutor
