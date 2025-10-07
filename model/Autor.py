62ab2b4d5345e57e1566c1c0bc0194bc49326fb8
João Pinheiro <jp.joaopedro9190@gmail.com>
2025-10-06 22:55:39 -0300

    modif autor para acesso ao bd

--- model/Autor.py
+++ model/Autor.py
@@ -1,28 +1,16 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from Base import Base
+from model.Base import Base
 
 class Autor(Base):
+    # nome da tabela
     __tablename__ = "autores"
 
+    # colunas da tabela
     idAutor = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String(60), nullable=False)
 
+    # relacionamentos da tabela
     mangas = relationship("Manga", back_populates="autor")
-    @property
-    def idAutor(self) -> int:
-        return self.__idAutor
-    
-    @idAutor.setter
-    def idAutor(self, idAutor: int) -> None:
-        self.__idAutor = idAutor
-    
-    @property
-    def nome(self) -> str:
-        return self.__nome
-    
-    @nome.setter
-    def nome(self, nome: str) -> None:
-        self.__nome = nome
 
     
