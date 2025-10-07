7df4fda8c3434c2b3b4ff7beeb8d8ab807e578ee
João Pinheiro <jp.joaopedro9190@gmail.com>
2025-10-06 23:11:12 -0300

    Add configMangaDaoImpl

---
+++ model/bd/ConfigMangaDaoImpl.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker
+from urllib.parse import quote_plus
+
+from model.ConfigManga import ConfigManga
+
+class ConfigMangaDaoImpl():
+    senha = quote_plus("Pipoca@04")
+    engine = create_engine(f"mysql+mysqlconnector://root:{senha}@localhost:3306/projetomanga", echo=False)
+
+    Session = sessionmaker(bind=engine)
+
+    def listarConfig(self) -> ConfigManga:
+        with self.Session() as session:
+            query = select(ConfigManga)
+            mangaAtual = session.scalars(query).one()
+
+        return mangaAtual
