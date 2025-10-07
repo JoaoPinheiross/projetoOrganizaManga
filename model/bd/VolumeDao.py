22f33d0ef7b2dbfae539b6d492a49f3847f1b5e4
João Pinheiro <jp.joaopedro9190@gmail.com>
2025-10-06 23:06:56 -0300

    Add volumeDao

---
+++ model/bd/VolumeDao.py
@@ -0,0 +1,12 @@
+from typing import List, Protocol
+
+from model.Volume import Volume
+
+class VolumeDao(Protocol):
+    # Lista os volumes do manga
+    def listarVolume(self) -> List[Volume]:
+        pass
+
+    # Pesquisa um volume pelo código
+    def pesquisarVolume(self, codVolume: int) -> Volume:
+        pass
