e017f7f2762fb5106f5e8502e4e2711fd2b63c10
João Pinheiro <jp.joaopedro9190@gmail.com>
2025-10-16 12:10:31 -0300

    Ajustando o Main

--- main.py
+++ main.py
@@ -3,20 +3,6 @@ from controller.MangaController import MangaController
 from view.TelaMenu import TelaMenu
 
 mangaController = MangaController()
-print(mangaController.listarMangas()[0].nome)
+#print(mangaController.listarMangas()[0].nome)
 telaMenu = TelaMenu(mangaController)
-#telaMenu.exibir()
-
-'''from pathlib import Path
-
-# Cria um objeto Path para a pasta desejada
-caminho = Path(r'C:UserspjoaDownloadscapManga')
-
-try:
-    print(f"Conteúdo da pasta '{caminho}':")
-    for item in caminho.iterdir():
-        # item.name extrai apenas o nome do arquivo/pasta
-        print(item.name)
-
-except FileNotFoundError:
-    print(f"Erro: O diretório '{caminho}' não foi encontrado.")'''
+telaMenu.exibir()
