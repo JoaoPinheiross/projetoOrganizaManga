38b7194c7e8ebd6231c51ff578fa85d1be2e2302
João Pinheiro <jp.joaopedro9190@gmail.com>
2025-12-21 22:36:25 -0300

    Voltando para MOBI

--- model/MangaService.py
+++ model/MangaService.py
@@ -205,7 +205,7 @@ class MangaService:
         # Comando a ser executado
         comando = [
             r"C:\mangas\KCC_c2e_9.1.0.exe",
-            "-m", "-s", "-c", "2", "-r", "1", "-p", "KPW", "-f", "EPUB",
+            "-m", "-s", "-c", "2", "-r", "1", "-p", "KPW", "-f", "MOBI",
             "-o", saida, entrada
         ]
         try:
